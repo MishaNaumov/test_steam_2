9d25bde32b01f4ea14444fd3bb2e821182d6f59e
Misha_Naumov <misha.naumov.999@gmail.com>
2024-08-02 22:02:32 +0300

    V1.0

--- tests/test_steam_2.py
+++ tests/test_steam_2.py
@@ -1,10 +1,11 @@
+import pytest
 import selenium.common
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from utils import JsonUtils
 
-UNIQUE_HOME_PAGE = "//a[@class='pulldown_desktop' and text() = 'Your Store']"
+UNIQUE_HOME_PAGE = "//*[@id='foryou_tab']//a[@class='pulldown_desktop']"
 WINDOW_SEARCH_ID = "store_nav_search_term"
 BUTTON_SEARCH = "//a[@id='store_search_link']//img"
 UNIQUE_SEARCH_PAGE = "//a[@id='sort_by_trigger']"
@@ -17,7 +18,15 @@ PRICE_GAME = \
     "and not(@class='discount_original_price')]"
 
 
-def test_steam_search_game(driver):
+@pytest.mark.parametrize(
+    "param",
+    [
+        pytest.param(("The Witcher", 10), id="The Witcher"),
+        pytest.param(("Fallout", 20), id="Fallout")
+    ]
+)
+def test_steam_search_game(driver, param):
+    name_game, qty = param
     wait = WebDriverWait(driver, JsonUtils.get_attribute("timeout"))
 
     def is_page_opened(locator):
@@ -29,53 +38,46 @@ def test_steam_search_game(driver):
 
     assert is_page_opened(UNIQUE_HOME_PAGE), "Home page not opened"
 
-    for item in JsonUtils.get_attribute("game"):
-        wait.until(EC.visibility_of_element_located
-                   ((By.ID, WINDOW_SEARCH_ID))).send_keys(item)
-        wait.until(EC.element_to_be_clickable
-                   ((By.XPATH, BUTTON_SEARCH))).click()
+    wait.until(EC.visibility_of_element_located((By.ID, WINDOW_SEARCH_ID)))\
+        .send_keys(name_game)
 
-        assert is_page_opened(UNIQUE_SEARCH_PAGE), "Store page not opened"
+    wait.until(EC.element_to_be_clickable((By.XPATH, BUTTON_SEARCH))).click()
 
-        wait.until(EC.element_to_be_clickable
-                   ((By.XPATH, FILTER))).click()
-        wait.until(EC.element_to_be_clickable
-                   ((By.ID, HIGHEST_PRICE_ID))).click()
+    assert is_page_opened(UNIQUE_SEARCH_PAGE), "Store page not opened"
 
-        WebDriverWait(driver, timeout=JsonUtils.get_attribute("timeout")
-                      , poll_frequency=0.07) \
-            .until(EC.presence_of_element_located((By.XPATH, LOADING)))
-        wait.until_not(EC.presence_of_element_located((By.XPATH, LOADING)))
+    wait.until(EC.element_to_be_clickable((By.XPATH, FILTER))).click()
 
-        def create_dict():
-            dict_game = {}
-            n = 1
-            qty = 0
-            if item == "The Witcher":
-                qty = 10
-            elif item == "Fallout":
-                qty = 20
+    wait.until(EC.element_to_be_clickable((By.ID, HIGHEST_PRICE_ID))).click()
 
-            for game in range(qty):
-                dict_game[wait.until(EC.presence_of_element_located
-                       ((By.XPATH, NAME_GAME.format(n)))).text] \
-                    = wait.until(EC.presence_of_element_located
-                       ((By.XPATH, PRICE_GAME.format(n)))).text
-                n += 1
+    WebDriverWait(driver, timeout=JsonUtils.get_attribute("timeout"),
+                  poll_frequency=0.07)\
+        .until(EC.presence_of_element_located((By.XPATH, LOADING)))
 
-            for game, price in dict_game.items():
-                dict_game[game] = float(price.replace(" руб", "").
-                                        replace(" pуб.", "").replace(",", "."))
-            return dict_game
+    wait.until_not(EC.presence_of_element_located((By.XPATH, LOADING)))
 
-        def sort_check():
-            temp_price = 1000000000000000000000
+    def create_dict():
+        dict_game = {}
+        n = 1
 
-            for price in create_dict().values():
-                if price <= temp_price:
-                    temp_price = price
-                else:
-                    return False
-            return True
+        for game in range(qty):
+            dict_game[wait.until(EC.presence_of_element_located
+                                 ((By.XPATH, NAME_GAME.format(n)))).text] = \
+                wait.until(EC.presence_of_element_located
+                           ((By.XPATH, PRICE_GAME.format(n)))).text
+            n += 1
 
-        assert sort_check(), "The filter doesn't work correctly"
+        for game, price in dict_game.items():
+            dict_game[game] = float(price[:-4].replace(",", "."))
+        return dict_game
+
+    def sort_check():
+        temp_price = 1000000000000000000000
+
+        for price in create_dict().values():
+            if price <= temp_price:
+                temp_price = price
+            else:
+                return False
+        return True
+
+    assert sort_check(), "The filter doesn't work correctly"
